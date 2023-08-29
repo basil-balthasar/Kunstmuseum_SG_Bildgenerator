6ebb4464f36aff144d3185933cf450ebcbe34272
basil-balthasar <batabasil@hotmail.com>
2023-08-29 22:55:57 +0200

    started with canvas positioning

---
+++ public/collage_generator.js
@@ -0,0 +1,9 @@
+let cnv
+
+function setup(){
+    cnv = createCanvas(300, 400)
+    cnv.style('display', 'block')
+    cnv.style('position', 'relative')
+    //cnv.position(200, 200)
+    background(250, 0, 200)
+}
