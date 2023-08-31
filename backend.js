882dc8f9bf015769c0415aec5076b2258281afc2
basil-balthasar <batabasil@hotmail.com>
2023-08-31 15:17:38 +0200

    removed img + completed backend

---
+++ backend.js
@@ -0,0 +1,38 @@
+//server requirements
+var http = require('http');
+var express = require("express");
+
+//serialport requirements + parser setup
+const SerialPort = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+const parsers = SerialPort.parsers;
+const parser = new ReadlineParser({ delimeter: "\r\n" });
+
+//open serialport
+const port = new SerialPort.SerialPort({
+  path: "/dev/cu.usbserial-110",
+  baudRate: 9600,
+  dataBits: 8,
+  parity: "none",
+  stopBits: 1,
+  flowControl: false,
+});
+port.pipe(parser);
+
+//open server on localhost
+var app = express();
+app.use(express.static("public"));
+var server = app.listen(3000);
+
+//io socket setup
+var io = require('socket.io')(server);
+io.sockets.on("connection", newConnection);
+function newConnection(socket){
+    console.log(socket.id);
+}
+
+//send serial data over io socket
+parser.on('data', function(data) {  
+    console.log('Received data from port: ' + data);
+    io.sockets.emit('data', data);
+});
