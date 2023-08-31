802926d932ade9b9b948c9122f68778ea99f08ea
basil-balthasar <batabasil@hotmail.com>
2023-08-31 16:30:50 +0200

    image layer draws

--- public/collageGenerator.js
+++ public/collageGenerator.js
@@ -13,8 +13,55 @@ function setup(){
     cnv.style('display', 'block')
     cnv.position(windowWidth/2-cnvX/2, windowHeight/2-cnvY/2)
     background(250, 0, 200)
+
+    //general setups
+    angleMode(DEGREES)
 }
 
 function windowResized() {
   cnv.position(windowWidth/2-cnvX/2, windowHeight/2-cnvY/2)
+}
+
+function draw(){
+  clear()
+  //background
+  if(backgroundImageOn == true){
+    //draw background image
+  }else{
+    background(backgroundColor, 255)
+  }
+
+  //draw layers in order of Z depth
+  for(let l = 0; l<4; l++){
+    switch(layerZOrder[l]){
+      case 0:
+        drawVg()
+        break;
+      case 1:
+        drawLayerOne()
+        break;
+      case 2:
+        drawLayerTwo()
+        break;
+      case 3:
+        drawLayerThree()
+        break;      
+    }
+  }
+}
+
+function drawVg(){
+
+}
+
+function drawLayerOne(){
+
+}
+
+function drawLayerTwo(){
+
+}
+
+function drawLayerThree(){
+
 }
--- public/dataTransform.js
+++ public/dataTransform.js
@@ -1,3 +1,12 @@
+//background values
+let backgroundImageOn = false
+let backgroundImage
+let backgroundImagePosition
+let backgroundImageScale
+let backgroundColor = [200, 0, 255]
+
+let layerZOrder = [0, 1, 2, 3]
+
 //get data from io sockets
 function getData(data){
     console.log("data connection open")
