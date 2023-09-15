1808bfdb2cafe128396e839490feacc1c1293745
basil-balthasar <batabasil@hotmail.com>
2023-09-15 21:32:08 +0200

    newCollage

--- public/History/history.js
+++ public/History/history.js
@@ -3,11 +3,14 @@ let lastFileName
 function setup(){
     createCanvas(windowWidth, windowHeight)
     frameRate(1)
+    background(0)
 }
 
 function draw(){
     lastFileName = localStorage.getItem("recentImageName")
     print(lastFileName)
     img = loadImage("./SavedImages/"+lastFileName)
+    fill(255)
+    rect(10,10,10,10)
     image(img, 0, 0, width, height)
 }
